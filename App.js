a78fb10ef57366e964ae8bdeff1f29f82b69f943
Praveen Kumar <praveenkumar@Praveens-MacBook-Pro.local>
2023-01-02 08:42:57 +0530

    .parcel-cache to gitignore

--- App.js
+++ App.js
@@ -1,3 +1,14 @@
-const heading = React.createElement('h2',{},'Hello World')
-        const root = ReactDOM.createRoot(document.getElementById('root'))
-        root.render(heading)
+import react from "react";
+import reactDom from "react-dom/client";
+
+const heading = react.createElement("h2", {}, "Hello World");
+const heading2 = react.createElement("h3", {}, "this is heading type 3");
+const div = react.createElement(
+  "div",
+  {
+    className: "test-div",
+  },
+  [heading, heading2]
+);
+const root = reactDom.createRoot(document.getElementById("root"));
+root.render(div);
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+
+# commands
+npx parcel index.html => to run the project locally
 # Theory
-# js append() vs appendChild()
+## js append() vs appendChild()
 Element.append() can append several nodes and strings, whereas Node.appendChild() can only append one node.
