ae24c545d481fc28e3f56ae53b6c828042a404bf
Praveen Kumar <praveenkumar@Praveens-MacBook-Pro.local>
2023-01-07 20:19:19 +0530

    static restaurant single card

--- App.js
+++ App.js
@@ -1,39 +1,78 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import logoimg from "./img/logo.jpg";
-import usericon from "./img/user.png";
-/*
-Create a Header Component from scratch using Functional Components with JSX
-Add a Logo on left
-Add a search bar in middle
-Add User icon on right
-Add CSS to make it look nice
-*/
-const logo = <img src={logoimg} className="img-fluid" alt="logo" />,
-  user = <img src={usericon} className="img-fluid" alt="user icon" />;
 
+/*
+Header => Logo, nav items, cart etc.
+Body => Restaurants List
+          Restaurant card
+            Restaurant image, name, cuisines, rating etc.
+Footer => Copyright info etc.
+
+*/
 const Header = () => {
   return (
     <header className="main-header">
-      <div>{logo}</div>
-
-      <form className="search-form">
-        <div className="search-wrap">
-          <input
-            type="search"
-            name="mainsearch"
-            id="search-input"
-            placeholder="Search..."
-            className="form-control"
-          />
-          <button type="submit" className="btn-submit"></button>
-        </div>
-      </form>
-
-      <div>{user}</div>
+      <a href="/" className="logo-a">
+        <img
+          src="https://yt3.ggpht.com/ytc/AMLnZu_EC-ECXAxRAixWGEfMsE1rdSoetBHyxmLNdtCB=s900-c-k-c0x00ffffff-no-rj"
+          alt="logo"
+        />
+      </a>
+      <ul className="main-nav">
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Contact</li>
+        <li>Cart</li>
+      </ul>
     </header>
   );
 };
 
+const Restaurant = () => {
+  return (
+    <article className="card">
+      <img
+        src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/bdcd233971b7c81bf77e1fa4471280eb"
+        alt="restaurant image"
+      />
+      <h3>KFC</h3>
+      <p>American, Snacks, Biryani</p>
+      <div style={{ display: "flex", gap: "10px" }}>
+        <span style={{ backgroundColor: "lightgreen", padding: " 0 4px" }}>
+          * 4.2
+        </span>
+        <span>41 min</span>
+      </div>
+      <p>FREE DELIVERY</p>
+    </article>
+  );
+};
+
+const Body = () => {
+  return (
+    <section>
+      <Restaurant />
+    </section>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer style={{ textAlign: "center" }}>
+      <h6>FoodVilla : Copyright 2023</h6>
+    </footer>
+  );
+};
+
+const AppComponent = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Header />);
+root.render(<AppComponent />);
