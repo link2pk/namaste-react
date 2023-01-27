ca705f0dbc122b6d75468be4433e98943bdafa31
PraveenKumar <link2pk@gmail.com>
2023-01-27 06:53:38 +0530

    useRestaurants hook for fetching restaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import useRestaurants from "../utils/useRestaurants";
 import NoRestaurant from "./NoRestaurant";
 import Restaurant from "./Restaurant";
 import Shimmer from "./Shimmer";
@@ -7,23 +8,7 @@ import Shimmer from "./Shimmer";
 const Body = () => {
   const [searchText, setSearchText] = useState("");
   const [restaurantCards, setRestaurantCards] = useState([]);
-  const [allRestaurant, setAllRestaurant] = useState([]);
-
-  useEffect(() => {
-    //fetch data here
-    fetchRestaurants();
-  }, []);
-
-  async function fetchRestaurants() {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.447326834373634&lng=76.9707901775837&page_type=DESKTOP_WEB_LISTING"
-    );
-    const json = await data.json();
-    const setData = json?.data?.cards[2]?.data?.data?.cards;
-    // console.log(setData);
-    setRestaurantCards(setData);
-    setAllRestaurant(setData);
-  }
+  const allRestaurant = useRestaurants(restaurantCards, setRestaurantCards);
 
   return allRestaurant?.length === 0 ? (
     <Shimmer />
---
+++ src/utils/useRestaurants.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+const useRestaurants = (restaurantCards, setRestaurantCards) => {
+  const [allRestaurant, setAllRestaurant] = useState([]);
+
+  useEffect(() => {
+    //fetch data here
+    fetchRestaurants();
+  }, []);
+
+  async function fetchRestaurants() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.447326834373634&lng=76.9707901775837&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    const setData = json?.data?.cards[2]?.data?.data?.cards;
+    // console.log(setData);
+    setRestaurantCards(setData);
+    setAllRestaurant(setData);
+  }
+  return allRestaurant;
+};
+
+export default useRestaurants;
